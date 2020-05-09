5a032662cd2be059cbe8d31e28b304c2275a9308
rosa lee <rosalee0116@gmail.com>
2020-05-09 20:23:40 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,26 +1,27 @@
-modeule Concerns::Findable
+module Concerns::Findable
 
-  def find_by_name(name)
-    all.detect {|x| x.name == name}
+      def find_by_name(name)
+         all.detect {|x| x.name == name}
+     end
+
+      def find_or_create_by_name(name)
+         if find_by_name(name) == nil
+            create(name)
+         else
+             find_by_name(name)
+         end
+     end
+
   end
 
-  def find_or_create_by_name(name)
-    if find_by_name(name) == nil
-      create(name)
-    else
-      find_by_name(name)
-    end
-  end
+  class Artist
+     extend Concerns::Findable
+ end
 
+  class Genre
+     extend Concerns::Findable
+ end
 
-class Artist
-  extend Concerns::Findable
-end
-
-class Genre
-  extend Concerns::Findable
-end
-
-class Song
-  extend Concerns::Findable
-end
+  class Song
+     extend Concerns::Findable
+ end
